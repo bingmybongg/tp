411998ce18d32532c5f044ce88124a5f999c4447
Tan Je-Deon <114093710+momentumnn@users.noreply.github.com>
2026-03-10 17:53:39 +0800

    Create ProgressRecord

---
+++ src/main/java/seedu/address/model/person/ProgressRecord.java
@@ -0,0 +1,65 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's address in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ */
+public class ProgressRecord {
+
+    public static final String MESSAGE_CONSTRAINTS = "Progress Records should contain a float, and is optional";
+
+    /*
+     * Validates that data is either a percentage.
+     * Example: 5%, 0.001%, 100%
+     */
+    public static final String VALIDATION_REGEX = "^(?:(?:\\d|[1-9]\\d)(?:\\.\\d+)?|100(?:\\.0+)?)\\s?%$";
+
+    public final String value;
+
+    /**
+     * Constructs a {@code ProgressRecord}.
+     *
+     * @param progress A progress.
+     */
+    public ProgressRecord(String progress) {
+        requireNonNull(progress);
+        checkArgument(isValidProgress(progress), MESSAGE_CONSTRAINTS);
+        value = progress;
+    }
+
+    /**
+     * Returns true if a given string is a valid email.
+     */
+    public static boolean isValidProgress(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ProgressRecord)) {
+            return false;
+        }
+
+        ProgressRecord otherProgress = (ProgressRecord) other;
+        return value.equals(otherProgress.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
